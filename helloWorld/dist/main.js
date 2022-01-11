ea37199988d680eb330a209bb38927f2d22649d1
raptor-cmd <josue091201@gmail.com>
2022-01-11 13:42:35 +0100

    Tipos de datos number, boolean y string

--- helloWorld/dist/main.js
+++ helloWorld/dist/main.js
@@ -1,2 +1,35 @@
 "use strict";
 console.log("Hola TypeScript con Platzi");
+//Number
+//Explícito
+let phone;
+phone = 1;
+phone = 542345;
+// Inferido
+let phoneNumber = 543213;
+phoneNumber = 1323;
+// phoneNumber = true; Error por tipo de dato
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o745;
+//Boolean
+// Explícito
+let isPro;
+isPro = true;
+// isPro = 1
+//Inferido
+let isUserPro = false;
+//String
+let username = "luxaviles";
+username = "Pepito";
+//Template String
+// Uso de back-tick `
+let userInfo;
+userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${username + " Juarez"}
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+console.log("userInfo", userInfo);
--- helloWorld/src/main.ts
+++ helloWorld/src/main.ts
@@ -1,1 +1,42 @@
 console.log("Hola TypeScript con Platzi");
+
+//Number
+//Explícito
+let phone: number;
+phone = 1;
+phone = 542345;
+
+// Inferido
+let phoneNumber = 543213;
+phoneNumber = 1323;
+// phoneNumber = true; Error por tipo de dato
+
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o745;
+
+//Boolean
+// Explícito
+let isPro: boolean;
+isPro = true;
+// isPro = 1
+
+//Inferido
+let isUserPro = false;
+
+//String
+let username: string = "luxaviles";
+username = "Pepito";
+
+//Template String
+// Uso de back-tick `
+let userInfo: string;
+userInfo = `
+    User Info:
+    username: ${username}
+    firstName: ${username + " Juarez"}
+    phone: ${phone}
+    isPro: ${isPro}
+`;
+
+console.log("userInfo", userInfo);
